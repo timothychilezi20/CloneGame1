884c4c62cbf662348235aa7dad50dcb8a7cb8eab
timothychilezi20 <timothychilezi@gmail.com>
2025-07-27 20:49:24 +0200

    Animation Transitions

--- CloneAssignment1/Assets/Scripts/PlayerMovement.cs
+++ CloneAssignment1/Assets/Scripts/PlayerMovement.cs
@@ -8,10 +8,13 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float moveSpeed = 5f;
     private Rigidbody2D rigidBody;
     private Vector2 moveInput;
+    public float jumpHeight; 
+    public Animator animator;
 
     private void Start()
     {
         rigidBody = GetComponent<Rigidbody2D>();
+        animator.GetComponent<Animator>(); 
     }
 
     void Update()
@@ -21,6 +24,46 @@ public class PlayerMovement : MonoBehaviour
 
     public void Move(InputAction.CallbackContext context)
     {
+        animator.SetBool("isWalking", true);
+
+        if(context.canceled)
+        {
+            animator.SetBool("isWalking", false); 
+            animator.SetFloat("LastInputX", moveInput.x);
+            animator.SetFloat("LastInputY", moveInput.y);  
+        }
+
         moveInput = context.ReadValue<Vector2>();
+        animator.SetFloat("InputX", moveInput.x);
+        animator.SetFloat("InputY", moveInput .y);
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        animator.SetBool("isJumping", true);
+
+        if (context.canceled)
+        {
+            animator.SetBool("isJumping", false);
+            animator.SetFloat("LastInputX", moveInput.x);
+            animator.SetFloat("LastInputY", moveInput.y);
+        }
+
+        rigidBody.velocity = new Vector2(rigidBody.velocity.x, jumpHeight);
+        animator.SetFloat("InputX", moveInput.x);
+        animator.SetFloat("InputY", moveInput.y);
+
+    }
+
+    public void Attack(InputAction.CallbackContext context)
+    {
+        animator.SetBool("isAttacking", true);
+
+        if (context.canceled)
+        {
+            animator.SetBool("isAttacking", false);
+            animator.SetFloat("LastInputX", moveInput.x);
+            animator.SetFloat("LastInputY", moveInput.y);
+        }
     }
 }
