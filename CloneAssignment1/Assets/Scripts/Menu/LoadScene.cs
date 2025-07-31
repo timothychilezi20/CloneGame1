8345944999387c27c08c7e67dcfdb0f00400c93e
Fifi-8916 <1986119@students.wits.ac.za>
2025-07-31 21:22:52 +0200

    Pause Menu
    Started working on Pause, just need to finish script and I should be done.

--- CloneAssignment1/Assets/Scripts/Menu/LoadScene.cs
+++ CloneAssignment1/Assets/Scripts/Menu/LoadScene.cs
@@ -7,6 +7,7 @@ public class LoadScene : MonoBehaviour
 {
     public string sceneName;//name of the scene 
     public GameObject controlsOptions;
+    public GameObject pauseOptions;
     public void changeScene()//change to the specified scene
     {
         SceneManager.LoadScene(sceneName);
@@ -26,5 +27,17 @@ public class LoadScene : MonoBehaviour
     public void closeControls()//close control panel
     {
         controlsOptions.SetActive(false);
+    }
+
+    public void pauseGame()//pause the game
+    {
+        Time.timeScale = 0f;//pause game
+        pauseOptions.SetActive(true);//open pause menu
+    }
+
+    public void resumeGame()
+    {
+        pauseOptions.SetActive(true);//open pause menu
+        Time.timeScale = 1f;//pause game
     }
 }
