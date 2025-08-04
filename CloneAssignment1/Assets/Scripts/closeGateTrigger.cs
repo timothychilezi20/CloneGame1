286365db88cd912294e1c3ec1e098db685d2f913
Fifi-8916 <1986119@students.wits.ac.za>
2025-08-04 12:15:34 +0200

    Start Timer
    Timer starts on trigger

--- CloneAssignment1/Assets/Scripts/closeGateTrigger.cs
+++ CloneAssignment1/Assets/Scripts/closeGateTrigger.cs
@@ -1,17 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class closeGateTrigger : MonoBehaviour
 {
     public GameObject closeGate;
-    public void OnTriggerEnter2D(Collider2D collision)
+    [SerializeField] TextMeshProUGUI timerText;
+    [SerializeField] float remainingTime;
+    private Collider2D thisCollider;
+    private bool runningTime;
+
+    private void Start()
     {
-        Debug.Log("Triggered by: " + collision.gameObject.name);
+        thisCollider = GetComponent<Collider2D>();
+        thisCollider.enabled = true;
+        runningTime = false;
+    }
+
+    void Update()
+    {
+        if (runningTime == true)
+        {
+            startTimer();
+        }
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)//when entering the trigger
+    {
+
         if (collision.CompareTag("Player"))
         {
             closeGate.SetActive(true);
+            runningTime = true;
+            Debug.Log("Timer Started");
+
+            thisCollider.enabled=false;//at the end once the time starts
         }
-        this.gameObject.SetActive(false);//at the end once the time starts
+ 
+    }
+
+    public void startTimer()
+    {
+        if (remainingTime > 0)
+        {
+            remainingTime -= Time.deltaTime;
+        }
+        else if (remainingTime < 0)
+        {
+            remainingTime = 0;
+        }
+        int minutes = Mathf.FloorToInt(remainingTime / 60);
+        int seconds = Mathf.FloorToInt(remainingTime % 60);
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
     }
 }
