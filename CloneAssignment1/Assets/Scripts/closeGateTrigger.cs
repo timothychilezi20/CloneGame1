03245abfb1c3fbcf17b56e0e2ac72628c5ce688a
Fifi-8916 <1986119@students.wits.ac.za>
2025-08-04 16:08:17 +0200

    Timer Working
    Timers works with a break in between

--- CloneAssignment1/Assets/Scripts/closeGateTrigger.cs
+++ CloneAssignment1/Assets/Scripts/closeGateTrigger.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Timers;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class closeGateTrigger : MonoBehaviour
@@ -10,20 +12,62 @@ public class closeGateTrigger : MonoBehaviour
     [SerializeField] float remainingTime;
     private Collider2D thisCollider;
     private bool runningTime;
+    private float elapsedTime;
+    private int timerCounter;
+    [SerializeField] float endBreakTime;
+    [SerializeField] float secondRemainingTime;
+    [SerializeField] bool secondRound = false;
 
     private void Start()
     {
         thisCollider = GetComponent<Collider2D>();
         thisCollider.enabled = true;
         runningTime = false;
+        secondRound = false;
     }
 
     void Update()
     {
         if (runningTime == true)
         {
-            startTimer();
+            countdownTimer();
+            //secondRound = true;
+            //runningTime = false;
         }
+
+        if (remainingTime == 0)
+        {
+            runningTime = false;
+            elapsedTime += Time.deltaTime;
+            int minutes = Mathf.FloorToInt(elapsedTime / 60);
+            int seconds = Mathf.FloorToInt(elapsedTime % 60);
+            timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+            if (elapsedTime >= endBreakTime && runningTime == false)
+            {
+                secondRound = true;
+                runningTime = true;
+                remainingTime = secondRemainingTime;
+                timerCounter++;
+                
+            }
+            if (secondRound == true && timerCounter>1)
+            {
+                runningTime = false;
+                timerText.text = string.Format("{0:00}:{1:00}", 0, 0);
+                //remainingTime = 0;
+                closeGate.SetActive(false);
+            }
+            //if (secondRound == true)
+            //{
+            //}
+
+            //countdownTimer();
+        }
+
+        
+
+
+
     }
 
     public void OnTriggerEnter2D(Collider2D collision)//when entering the trigger
@@ -40,7 +84,7 @@ public class closeGateTrigger : MonoBehaviour
  
     }
 
-    public void startTimer()
+    public void countdownTimer()
     {
         if (remainingTime > 0)
         {
@@ -53,5 +97,25 @@ public class closeGateTrigger : MonoBehaviour
         int minutes = Mathf.FloorToInt(remainingTime / 60);
         int seconds = Mathf.FloorToInt(remainingTime % 60);
         timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+
+    public void breaktimer()
+    {
+        //if (remainingTime > 0)
+        //{
+
+        //}
+        //else if (remainingTime < 0)
+        //{
+        //    remainingTime = 0;
+        //}
+        //elapsedTime += Time.deltaTime;
+        //int minutes = Mathf.FloorToInt(elapsedTime / 60);
+        //int seconds = Mathf.FloorToInt(elapsedTime % 60);
+        //timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+        //if (elapsedTime >= endBreakTime)
+        //{
+        //    elapsedTime = remainingTime;
+        //}
     }
 }
